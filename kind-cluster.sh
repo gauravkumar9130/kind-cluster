6eb7fc3f5d7af9482e08761cfd2448d0506557d0
gauravkumar9130 <55126211+gauravkumar9130@users.noreply.github.com>
2025-09-09 15:14:27 +0530

    Update kind-cluster.sh

--- kind-cluster.sh
+++ kind-cluster.sh
@@ -1,12 +1,15 @@
-wget -O /etc/yum.repos.d/docker-ce.repo https://download.docker.com/linux/centos/docker-ce.repo
-dnf install docker-ce -y
-systemctl start docker
-systemctl enable docker
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-chmod +x kubectl
-mv kubectl /bin/
-[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.30.0/kind-linux-amd64
-chmod +x ./kind
-mv kind /bin/
-kind create cluster --config kind-example-config.yaml
-kubectl get nodes
+#wget -O /etc/yum.repos.d/docker-ce.repo https://download.docker.com/linux/centos/docker-ce.repo
+#dnf install docker-ce -y
+#systemctl start docker
+#systemctl enable docker
+#curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+#chmod +x kubectl
+#mv kubectl /bin/
+#[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.30.0/kind-linux-amd64
+#chmod +x ./kind
+#mv kind /bin/
+#kind create cluster --config kind-example-config.yaml
+#kubectl get nodes
+#docker ps 
+We will see three nodes 
+#docker exec -it <CONTAINER NAME> -- bash -> to go inside any node
